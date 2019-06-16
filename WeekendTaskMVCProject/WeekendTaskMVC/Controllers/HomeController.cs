5d6590cffdc073f51baa109b3c19a6f19129c7cf
- <HeydarRa@code.edu.az>
2019-06-16 16:00:58 +0400

    done

---
+++ WeekendTaskMVCProject/WeekendTaskMVC/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeekendTaskMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Home1()
+        {
+            return View();
+        }
+        public ActionResult Home2()
+        {
+            return View();
+        }
+        public ActionResult Home3()
+        {
+            return View();
+        }
+        public ActionResult Home4()
+        {
+            return View();
+        }
+        public ActionResult Home5()
+        {
+            return View();
+        }
+        public ActionResult Home6()
+        {
+            return View();
+        }
+    }
+}
